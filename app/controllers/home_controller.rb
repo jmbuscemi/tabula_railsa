7ed729407dd4d614f06202be70775e5d68fbe5d2
John M Buscemi <jmbuscemi@gmail.com>
2015-06-17 09:17:53 -0400

    Add session so users can't take survey twice.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :check_session, only: [:show, :create]
 
   def new
     @survey = Survey.new
@@ -9,6 +10,7 @@ class HomeController < ApplicationController
 
     respond_to do |format|
       if @survey.save
+        session[:survey_id] = @survey.id
         format.html { redirect_to home_path(@survey), notice: 'Survey was successfully created.' }
         format.json { render :show, status: :created, location: @survey }
       else
@@ -23,9 +25,16 @@ class HomeController < ApplicationController
   end
 
 
+  private
+    def survey_params
+      params.require(:survey).permit(:name, :chocolate, :rainbow, :puppies, :cash)
+    end
 
-  private def survey_params
-    params.require(:survey).permit(:name, :chocolate, :rainbow, :puppies, :cash)
-  end
+    def check_session
+      unless session[:survey_id] == nil
+        flash.now[:notice] = "You have already submitted a survey"
+        redirect_to root_path
+      end
+    end
 
 end
